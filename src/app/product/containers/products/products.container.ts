e2006e52486e28ee62e517a43c6f253de862653a
cesar puentes <caps2695@gmail.com>
2020-11-12 00:48:27 -0500

    patron smart and dumb

---
+++ src/app/product/containers/products/products.container.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../../../core/models/product.model';
+import { ProductsService } from '../../../core/services/products/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.container.html',
+  styleUrls: ['./products.container.scss'],
+})
+// tslint:disable-next-line: component-class-suffix
+export class ProductsContainer implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  clickProduct(id: number): void {
+    console.log('product');
+    console.log(id);
+  }
+
+  fetchProducts(): void {
+    this.productsService.getAllProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
