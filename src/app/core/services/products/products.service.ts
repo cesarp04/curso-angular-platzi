45fdd1c02fc6f7ad39cdee8a64bec7e6890b3294
cesar puentes <caps2695@gmail.com>
2020-11-12 19:04:05 -0500

    tipado en peticiones

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { Product } from './../../models/product.model';
 
 import { environment } from './../../../../environments/environment';
+import { Observable } from 'rxjs';
+
+interface User {
+  email: string;
+  genero: string;
+  phone: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getAllProducts() {
     return this.http.get<Product[]>(`${environment.url_api}/products`);
@@ -32,5 +37,10 @@ export class ProductsService {
 
   deleteProduct(id: string) {
     return this.http.delete(`${environment.url_api}/products/${id}`);
+  }
+  getRandomUsers(): Observable<User[]> {
+    return this.http
+      .get('https://randomuser.me/api/?results=2')
+      .pipe(map((response: any) => response.results as User[]));
   }
 }
