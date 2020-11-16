bb03e4801c10839b5d0397ee6d8bd07a5a270a85
cesar puentes <caps2695@gmail.com>
2020-11-16 12:15:17 -0500

    pruebas a providers  y servicios http

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,12 +1,72 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { ProductsService } from './products.service';
+import { environment } from 'src/environments/environment';
 
-describe('ProductsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('ProductsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let service: ProductsService;
+
+  // beforeeach antes de cada prueba
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(ProductsService);
+  });
 
   it('should be created', () => {
-    const service: ProductsService = TestBed.get(ProductsService);
     expect(service).toBeTruthy();
+  });
+
+  describe('tests for getAllProducts', () => {
+    it('should return products', () => {
+      // arrange
+      const expectData = [
+        {
+          id: '1',
+          title: 'asas',
+          price: 1212,
+          description: 'asas',
+          image: 'img/img.jpg',
+        },
+        {
+          id: '2',
+          title: 'sdfdf',
+          price: 1212,
+          description: 'asas',
+          image: 'img/img.jpg',
+        },
+      ];
+      let dataError, dataResponse;
+      // act
+      console.log(service);
+      service.getAllProducts().subscribe(
+        (response) => {
+          dataResponse = response;
+        },
+        (error) => {
+          dataError = error;
+        }
+      );
+      const req = httpTestingController.expectOne(
+        `${environment.url_api}/products`
+      );
+      // req.flush devuelve la data previamente mockeada
+      req.flush(expectData);
+      // assert
+      expect(dataResponse.length).toEqual(2);
+      expect(req.request.method).toEqual('GET');
+      expect(dataError).toBeUndefined();
+    });
   });
 });
